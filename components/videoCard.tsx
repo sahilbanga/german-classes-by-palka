44bd868b2f744942eab0c1e8d55730e08fbd8662
bangas <sahil.banga@ril.com>
2023-11-22 18:16:44 +0530

    define interface for data

--- components/videoCard.tsx
+++ components/videoCard.tsx
@@ -1,6 +1,23 @@
 import Link from "next/link";
 
-export default function VideoCard({data}) {
+
+interface VideoCardProps {
+    data: {
+        snippet: {
+            resourceId: {
+                videoId: string;
+            };
+            thumbnails: {
+                medium: {
+                    url: string;
+                };
+            };
+            title: string;
+        };
+    };
+}
+
+export default function VideoCard({data}:VideoCardProps) {
     return (
         <li>
             <Link href={`https://www.youtube.com/watch?v=${data?.snippet.resourceId?.videoId}`} target={'_blank'}>
