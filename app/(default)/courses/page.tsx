f3478670a700a4c2a2aa357ae34bc4aa42bcf91e
bangas <sahil.banga@ril.com>
2023-11-22 11:37:15 +0530

    initial commit

---
+++ app/(default)/courses/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'Course Page',
+    description: 'Course Page description',
+}
+
+export default function Courses() {
+    return (
+        <>
+            Courses
+        </>
+    )
+}
