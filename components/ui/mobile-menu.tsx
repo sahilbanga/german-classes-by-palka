04daa3a1de9013be32a6b9e4aecff8c0be28206b
bangas <sahil.banga@ril.com>
2023-11-23 10:02:35 +0530

    modify nav links

--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -61,16 +61,35 @@ export default function MobileMenu() {
       >
         <ul className="bg-gray-800 px-4 py-2">
           <li>
-            <Link href="/signin" className="flex font-medium w-full text-purple-600 hover:text-gray-200 py-2 justify-center" onClick={() => setMobileNavOpen(false)}>
-              Sign in
+            <Link
+                href="/"
+                className={`btn-sm ml-3 text-white`}
+            >
+              Home
             </Link>
           </li>
           <li>
             <Link
-              href="/signup"
-              className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out" onClick={() => setMobileNavOpen(false)}
+                href="/about"
+                className={`btn-sm ml-3 text-white`}
             >
-              Sign up
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+                href="/courses"
+                className={`btn-sm ml-3 text-white`}
+            >
+              Courses
+            </Link>
+          </li>
+          <li>
+            <Link
+                href="/videos"
+                className={`btn-sm ml-3 text-white`}
+            >
+              Videos
             </Link>
           </li>
         </ul>
