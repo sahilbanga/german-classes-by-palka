5e0ea36a871ff4521f4fcf8ab8482022ec86f8e2
bangas <sahil.banga@ril.com>
2023-11-23 09:37:06 +0530

    dev of courses page

--- app/(default)/courses/page.tsx
+++ app/(default)/courses/page.tsx
@@ -5,8 +5,74 @@ export const metadata = {
 
 export default function Courses() {
     return (
-        <>
-            Courses
-        </>
+        <section className="relative content-none">
+            <div className="max-w-6xl mx-auto px-4 sm:px-6">
+                <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+                    {/* Page header */}
+                    <div className="max-w-4xl mx-auto pb-12 md:pb-20">
+                        <h1 className="h1">German Language Learning Levels</h1>
+                        <div className={'mt-5'}>
+                            <h2 className="h3 text-purple-600 pb-2">Level A1 – Basic knowledge</h2>
+                            <p>
+                                This one is the beginner level. After completing this level, you would be able to understand and use simple words and phrases. You become able to introduce yourself to others. Moreover, you can answer and ask questions related to personal details like people you know, things you have, and where you live. You can easily interact with people in a simple way if the other person you are talking to speaks clearly and slowly.
+                            </p>
+                        </div>
+
+
+                        <div className={'mt-5'}>
+                            <h2 className="h3 text-purple-600 pb-2">Level A2</h2>
+                            <p>
+                                After successfully completing the A2 level, you would be able to understand long sentences. Moreover, you can understand the frequently used phrases and expressions associated with immediate relevance like family information, personal information, employment, local geography, and shopping.
+
+                                You become able to communicate in routine and simple tasks that require a direct and basic information exchange on routine matters. You can describe different aspects of the immediate environment, background, and requirements in simple terms.
+                            </p>
+                        </div>
+
+
+                        <div className={'mt-5'}>
+                            <h2 className="h3 text-purple-600 pb-2">Level B1 – Intermediate Language Use</h2>
+                            <p>
+                                At the end of the B1 level, you become able to understand the main point of standard language in matters that are usually encountered in school, work, etc. You can easily deal with situations that can possibly arise while traveling in areas where German is used.
+
+                                After completing this level, you can produce connected and simple sentences on different topics that are of personal interest. You become able to describe events, hopes, dreams, ambitions, and experiences. Moreover, you can briefly explain your plans and opinions.
+                            </p>
+                        </div>
+
+
+                        <div className={'mt-5'}>
+                            <h2 className="h3 text-purple-600 pb-2">Level B2</h2>
+                            <p>
+                                Once you complete this level, you can understand the primary ideas of complex sentences on abstract and concrete topics, including discussions in your core field. You can communicate fluently while having normal conversations with native speakers.
+
+                                You can easily make detailed and clear texts on different topics and subjects. In addition to this, you can explain your viewpoint on a particular topic, including the pros and cons of available options.
+                            </p>
+                        </div>
+
+                        <div className={'mt-5'}>
+                            <h2 className="h3 text-purple-600 pb-2">Level C1 – Advanced Language Use</h2>
+                            <p>
+
+                                If you complete this level, it means you can easily comprehend various long and demanding texts. Moreover, you can understand the implicit meanings. You become able to express yourself fluently without searching for expressions. You can easily use the German language effectively and flexibly for professional, academic, as well as social purposes.
+
+                                After completing this level, you can easily make well-structured, detailed, as well as clear sentences on complicated topics. You become able to make controlled use of organizational connectors, patterns, and cohesive devices.
+                            </p>
+                        </div>
+
+                        <div className={'mt-5'}>
+                            <h2 className="h3 text-purple-600 pb-2">Level C2</h2>
+                            <p>
+                                After reaching this level, you can easily understand everything that is read or heard. You become able to summaries information from a wide range of spoken and written sources. Moreover, you can easily reconstruct arguments in a coherent presentation.
+
+                                Once you complete this level, you can spontaneously express yourself in a precise and fluent manner.
+
+                                You can not pursue a course or degree without having proper knowledge of the German language. For taking admission to a German university and for survival in the country, it is essential to learn German. It is a commonly used language in Europe. So make sure to be fully prepared before pursuing studies in German.
+                            </p>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </section>
     )
 }
